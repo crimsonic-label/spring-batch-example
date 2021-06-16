d3dc72cb66c2fb46f3b1a66807d1e9c666213457
Andrzej D <adrozdow@o2.pl>
2021-06-16 17:28:00 +0200

    use of callable tasklet adapter

--- src/main/java/atd/test/springbatchexample/DailyJobTimestamper.java
+++ src/main/java/atd/test/springbatchexample/DailyJobTimestamper.java
@@ -4,14 +4,15 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersIncrementer;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DailyJobTimestamper implements JobParametersIncrementer {
 
     @Override
     public JobParameters getNext(JobParameters jobParameters) {
         return new JobParametersBuilder(jobParameters)
-                .addDate("currentDate", new Date())
+                .addString("currentDate", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm")))
                 .toJobParameters();
     }
 }
--- src/main/java/atd/test/springbatchexample/SpringBatchExampleApplication.java
+++ src/main/java/atd/test/springbatchexample/SpringBatchExampleApplication.java
@@ -1,29 +1,26 @@
 package atd.test.springbatchexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.CompositeJobParametersValidator;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
+import java.util.concurrent.Callable;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchExampleApplication {
+
+    Logger logger = LoggerFactory.getLogger(SpringBatchExampleApplication.class);
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -31,56 +28,32 @@ public class SpringBatchExampleApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public JobParametersValidator defaultValidator() {
-        // only parameter existence is to be checked with DefaultJobParametersValidator
-        // no other parameters are allowed, only fileName and name
-        DefaultJobParametersValidator validator = new DefaultJobParametersValidator(
-                new String[]{"fileName"},
-                // accept run.id parameter created by incrementer
-                new String[]{"name", "currentDate"});
-        validator.afterPropertiesSet();
-        return validator;
-    }
-
-    @Bean
-    public JobParametersValidator compositeValidator() throws Exception {
-        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
-        validator.setValidators(Arrays.asList(new ParametersValidator(), defaultValidator()));
-        validator.afterPropertiesSet();
-        return validator;
-    }
-
-    /**
-     * promotion listener look for key "name" in step's execution context
-     * when it is found after the step is successfully completed,
-     * it will be copied into job execution context.
-     * When not found, nothing happens
-     *
-     * @return the listener
-     */
-    @Bean
-    public StepExecutionListener promotionListener() {
-        ExecutionContextPromotionListener listener = new ExecutionContextPromotionListener();
-        listener.setKeys(new String[] {"name"});
-        return listener;
-    }
-
-    @Bean
-    public Step step() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new HelloWorldTasklet())
-                .listener(promotionListener())
-                .build();
-    }
-
-    @Bean
-    public Job job() throws Exception {
-        return jobBuilderFactory.get("basicJob")
-                .start(step())
-                .validator(compositeValidator())
+    public Job collableJob() {
+        return jobBuilderFactory.get("callableStep")
                 .incrementer(new DailyJobTimestamper())
-                .listener(new JobLoggerListener())
+                .start(callableStep())
                 .build();
+    }
+
+    @Bean
+    public Step callableStep() {
+        return stepBuilderFactory.get("callableStep")
+                .tasklet(callableTasklet())
+                .build();
+    }
+
+    @Bean
+    public CallableTaskletAdapter callableTasklet() {
+        CallableTaskletAdapter callableTaskletAdapter = new CallableTaskletAdapter();
+        callableTaskletAdapter.setCallable(callableObject());
+        return callableTaskletAdapter;
+    }
+
+    private Callable<RepeatStatus> callableObject() {
+        return () -> {
+            logger.info("This is run in another thread: {}", Thread.currentThread().getName());
+            return RepeatStatus.FINISHED;
+        };
     }
 
     /**
