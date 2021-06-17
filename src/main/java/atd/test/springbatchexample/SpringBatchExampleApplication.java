6f91c791abaacdceaa9a0f428b9994369c5029e2
Andrzej D <adrozdow@o2.pl>
2021-06-17 19:25:37 +0200

    conditional job

--- src/main/java/atd/test/springbatchexample/SpringBatchExampleApplication.java
+++ src/main/java/atd/test/springbatchexample/SpringBatchExampleApplication.java
@@ -2,24 +2,19 @@ package atd.test.springbatchexample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.CompletionPolicy;
-import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
-import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.UUID;
-import java.util.stream.IntStream;
+import java.util.Random;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -33,48 +28,48 @@ public class SpringBatchExampleApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job chunkBasedJob() {
-        return jobBuilderFactory.get("chunkBasedJob")
+    public Job job() {
+        return jobBuilderFactory.get("conditionalJob")
                 .incrementer(new DailyJobTimestamper())
-                .start(chunkStep())
+                .start(firstStep())
+                .on(ExitStatus.FAILED.getExitCode()).to(failureStep())
+                .from(firstStep()).on("*").to(successStep())
+                .end()
                 .build();
     }
 
     @Bean
-    public Step chunkStep() {
-        return stepBuilderFactory.get("chunkStep")
-                .<String, String>chunk(completionPolicy())
-                .reader(itemReader())
-                .writer(itemWriter())
-                .listener(new LoggingStepStartStopListener())
+    public Step firstStep() {
+        Random random = new Random();
+
+        return stepBuilderFactory.get("firstStep")
+                .tasklet((contribution, chunkContext) -> {
+                    if(random.nextBoolean()) {
+                        return RepeatStatus.FINISHED;
+                    }
+                    throw new RuntimeException("This is failure");
+                })
                 .build();
     }
 
     @Bean
-    public CompletionPolicy completionPolicy() {
-        CompositeCompletionPolicy policy = new CompositeCompletionPolicy();
-        policy.setPolicies(new CompletionPolicy[]{
-                new RandomChunkSizePolicy(),
-                //new SimpleCompletionPolicy(1000),
-                new TimeoutTerminationPolicy(1)
-        });
-        return policy;
+    public Step successStep() {
+        return stepBuilderFactory.get("successStep")
+                .tasklet((contribution, chunkContext) -> {
+                    logger.info("Success!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 
     @Bean
-    public ListItemReader<String> itemReader() {
-        return new ListItemReader<>(IntStream.range(0, 100000)
-                .collect(() -> new ArrayList<>(100000),
-                        (result, item) -> result.add(UUID.randomUUID().toString()),
-                        ArrayList::addAll));
-    }
-
-    @Bean
-    public ItemWriter<String> itemWriter() {
-        return items -> {
-            items.forEach(item -> logger.info(">> current item = {}", item));
-            logger.info("chunk finished with {} items", items.size());
-        };
+    public Step failureStep() {
+        return stepBuilderFactory.get("failureStep")
+                .tasklet((contribution, chunkContext) -> {
+                    logger.warn("Failure");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 
     /**
